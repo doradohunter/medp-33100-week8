113ae2adaee860be7665918f860b35b9fc862f33
Rochelle Williams <rochelle.williams80@myhunter.cuny.edu>
2024-10-21 14:01:48 -0400

    main hw done
    to add next:
    more styling
    make add nations run once
    add ideals
    maybe add links to archons

--- script.js
+++ script.js
@@ -1,36 +1,93 @@
-// /*fetch data from json file*/
-// function getData() {
-//     fetch('characters.json')
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .then((data) => {
-//             const collection = document.querySelector('.collection');
-//             for (let i = 0; i < data.length; i++) {
-//                 const element = document.createElement('div');
-//                 collection.appendChild(element);
-//                 const character = new Character(element, data[i].icon, data[i].name, data[i].rarity, data[i].vision, data[i].weapon, data[i].nation);
-//                 character.showCharacter();
-//             }
-//         })
-// }
-const api_url = 'https://genshin.jmp.blue/characters'
+const api_url = 'https://genshin.jmp.blue/';
+const collection = document.querySelector('.collection');
+const nations = document.querySelector('.nations');
+
 async function getAllCharacters() {
-    const response = await fetch(api_url + '/all');
+    const response = await fetch(api_url + 'characters/all');
     const data = await response.json();
-    const collection = document.querySelector('.collection');
-    for (let i = 0; i < data.length; i++) {
-        const element = document.createElement('div');
-        collection.appendChild(element);
-        const character = new Character(element, data[i].id, data[i].name, data[i].rarity, data[i].vision, data[i].weapon, data[i].nation);
-        console.log(data[i].id)
-        character.showCharacter();
-    }
     return data;
 }
 
-getAllCharacters();
+async function getNations() {
+    const response = await fetch(api_url + 'nations/all')
+    const data = await response.json();
+    return data;
+}
 
+getAllCharacters()
+    .then((data)=>{
+        data.forEach(chara => {
+            showAllCharacters(chara);
+        });
+    })
+
+function showAllCharacters(chara){
+    const element = document.createElement('div');
+    collection.appendChild(element);
+    const character = new Character(element, chara.id, chara.name, chara.rarity, chara.vision, chara.weapon, chara.nation);
+    character.showCharacter();
+}
+
+function showNations(nat){
+    const element = document.createElement('div');
+    nations.appendChild(element);
+    const nation = new Nation (element, nat.id, nat.name, nat.element, nat.archon, nat.controllingEntity)
+    nation.showNation();
+}
+
+const natButton = document.querySelector('#nationButton');
+
+natButton.addEventListener('click', async () => {
+    getNations()
+        .then((data)=>{
+            data.forEach(nation=>{
+                showNations(nation);
+            })
+        })
+})
+
+class Nation {
+    constructor(element, id, name, ele, archon, control){
+        this.element = element;
+        this.id = id;
+        this.name = name;
+        this.ele = ele;
+        this.archon = archon;
+        this.control = control;
+
+        this.element.classList.add('box', this.ele);
+    }
+
+    showNation(){
+        this.element.innerHTML = '';
+
+        const iconElement = document.createElement('img');
+        iconElement.classList.add('icon');
+        iconElement.src = api_url + 'nations/' + this.id + '/icon';
+
+        const nameElement = document.createElement('p');
+        nameElement.classList.add('name');
+        nameElement.innerText = this.name;
+
+        const eleElement = document.createElement('p');
+        eleElement.classList.add('ele');
+        eleElement.innerText = 'Element: ' + this.ele;
+
+        const archonElement = document.createElement('p');
+        archonElement.classList.add('archon');
+        archonElement.innerText = 'Archon: ' + this.archon;
+
+        const controlElement = document.createElement('p');
+        controlElement.classList.add('control');
+        controlElement.innerText = 'Control­ling En­ti­ty: ' + this.control;
+
+        this.element.appendChild(iconElement);
+        this.element.appendChild(nameElement);
+        this.element.appendChild(eleElement);
+        this.element.appendChild(archonElement);
+        this.element.appendChild(controlElement);
+    }
+}
 
 /*Class for Character*/
 class Character {
@@ -43,7 +100,7 @@ class Character {
         this.weapon = weapon;
         this.nation = nation;
 
-        this.element.classList.add('character', this.vision);
+        this.element.classList.add('box', this.vision);
 
     }
 
@@ -53,7 +110,7 @@ class Character {
         const iconElement = document.createElement('img');
         const id = this.icon.toLowerCase();
         iconElement.classList.add('icon');
-        iconElement.src = api_url + '/' + id + '/icon-big';
+        iconElement.src = api_url + 'characters/' + id + '/icon-big';
 
         const nameElement = document.createElement('p');
         nameElement.classList.add('name');
@@ -71,7 +128,6 @@ class Character {
             default:
                 break;
         }
-        //rareElement.innerText = 'Rarity: ' + this.rarity;
 
         const visionElement = document.createElement('p');
         visionElement.classList.add('vision');
@@ -97,4 +153,3 @@ class Character {
         this.element.appendChild(nationElement);
     };
 };
-
